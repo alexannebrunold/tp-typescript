7c8ec5045612ca0710f8a8edaef3fe7a1b58660f
Alexanne Brunold <alexanne.brunold@hetic.net>
2020-05-19 17:43:30 +0200

    exo 1 & 2

--- src/js/script.ts
+++ src/js/script.ts
@@ -1,1 +1,14 @@
 console.log('Hello World');
+
+function printStudentName(name : string) : void {
+    const formattedName : string = name.toUpperCase();
+    console.log(`L'étudiant s'appelle ${formattedName}`);
+  }
+  printStudentName("Arnaud")
+
+  function printAlphabetically(items : string[]) : void {
+    const sortedItems : string[] = items.sort();
+    sortedItems.forEach(item => console.log(item));
+  }
+
+  printAlphabetically(["Salut", "Bonjour", "Coucou"])
